cf9ebeb4b37538dc1c4747f7bd8d11e3bbf6a144
Jose <77968901+jserranodev@users.noreply.github.com>
2022-03-11 16:17:18 +0100

    Implementado servicio REST listado asignaturas

---
+++ src/main/java/com/nttdata/webasignaturas/restcontroller/AsignaturaController.java
@@ -0,0 +1,25 @@
+package com.nttdata.webasignaturas.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nttdata.webasignaturas.repository.entity.Asignatura;
+import com.nttdata.webasignaturas.service.AsignaturaService;
+
+@RestController
+@RequestMapping("/api/lista-asignaturas")
+public class AsignaturaController {
+
+	@Autowired
+	AsignaturaService asignaturaService;
+	
+	@GetMapping
+	public List<Asignatura> obtenerListaAsignaturas(){
+		return asignaturaService.listar();
+	}
+	
+}
--- src/main/java/com/nttdata/webasignaturas/service/impl/AsignaturaServiceImpl.java
+++ src/main/java/com/nttdata/webasignaturas/service/impl/AsignaturaServiceImpl.java
@@ -3,18 +3,20 @@ package com.nttdata.webasignaturas.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.nttdata.webasignaturas.repository.AsignaturaRepoJPA;
 import com.nttdata.webasignaturas.repository.entity.Asignatura;
 import com.nttdata.webasignaturas.service.AsignaturaService;
 
-@Component
+@Service
 public class AsignaturaServiceImpl implements AsignaturaService{
 
 	@Autowired
 	AsignaturaRepoJPA asignaturaRepoJpa;
 	
+	
+	// Consulta médiante métodos ya existentes en JPA
 	@Override
 	public List<Asignatura> listar() {
 		// TODO Auto-generated method stub

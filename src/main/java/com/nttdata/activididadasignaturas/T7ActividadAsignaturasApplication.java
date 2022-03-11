c0cdfa04da53d55fcfe3d4463f4bd77d50918202
Jose <77968901+jserranodev@users.noreply.github.com>
2022-03-11 12:48:41 +0100

    Commit inicial

---
+++ src/main/java/com/nttdata/activididadasignaturas/T7ActividadAsignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.activididadasignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T7ActividadAsignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T7ActividadAsignaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/activididadasignaturas/T7ActividadAsignaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.activididadasignaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T7ActividadAsignaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
